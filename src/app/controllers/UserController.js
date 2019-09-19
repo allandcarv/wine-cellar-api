4483d6630b3dac825b794c723eaabe68fb17dcfe
Allan Carvalho <allandcarv@gmail.com>
2019-09-19 12:20:13 -0300

    UserController.store Data Validation

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,13 +1,31 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+      confirmPassword: Yup.string()
+        .required()
+        .oneOf([Yup.ref('password')]),
+      admin: Yup.boolean(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Data Input Validation Failed ' });
+    }
+
     const checkUser = await User.findOne({ where: { email: req.body.email } });
 
     if (checkUser) {
       return res
         .status(400)
-        .json({ error: 'This email has already registered. ' });
+        .json({ error: 'This email has already been registered. ' });
     }
 
     const { id, name, email, admin } = await User.create(req.body);
