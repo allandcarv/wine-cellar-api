336580eefdcd91551eae6890dc93e6d9c4832efc
Allan Carvalho <allandcarv@gmail.com>
2019-09-19 13:22:28 -0300

    Multer Config

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
@@ -7,5 +8,7 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
